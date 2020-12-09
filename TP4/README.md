d38cd3b3313291a7f066efe1be2852671b188d6e
alcbeatriz <beatriz.cristina@ufvjm.edu.br>
2020-12-09 12:59:36 -0300

    update

--- TP4/README.md
+++ TP4/README.md
@@ -4,5 +4,6 @@ Alunos: Amanda Lopes - Beatriz Alcantara - Caliny Basilio - Pietro Henrique
 _______________________
 #TP4
 _____________________
-OBS: Feito no Windows10 x64 - VsCode
+OBS: Feito no Windows10 x64 - VsCode python 3.9 - testado no Linux.
+
 rodar o arquivo main e depois o arquivo cliente - Run Current file in Interactive Window
---
+++ TP4/tp/script.py
@@ -0,0 +1,15 @@
+import json
+def escrever_json(dados): #criar o arquivo
+    with open('meu_arquivo.json', 'w',encoding='utf8') as f: #cria o arquivo json
+        json.dump(dados,f,ensure_ascii=False, sort_keys=True, indent=4,separators=(',',':')) #cria garantindo alguns paramentros para a organização da informação
+
+meu_dict ={ #informações para o json
+    'Disciplina': 'Redes de Computadores 2',
+    "Professor": 'Alessandro Vivas',
+    "Aluno1":'Beatriz',
+    "Aluno2":'Amanda', 
+    "Aluno3":'Caliny',
+    "Aluno4": 'Pietro'
+}
+
+escrever_json(meu_dict) #gerar o arquivo
